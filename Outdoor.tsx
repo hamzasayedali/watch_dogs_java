7c4edbc5f96e1f072d385b8ef8d5efd31fc93fd9
Hamza Sayed-Ali <hamzasayedali@gmail.com>
2019-11-10 17:11:24 -0500

    Last project files to add

---
+++ Outdoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Outdoor" tilewidth="32" tileheight="32" tilecount="81" columns="9">
+ <image source="Industrial pack/tiles/industrial-tileset.png" width="288" height="288"/>
+</tileset>
